eac7a59e6fc4c7ad7d6d54f7d32d893deb60a351
Donovan <wynot1402@gmail.com>
2018-12-10 13:34:19 +0100

    Début adaptation smartphone et remplissage contenu

--- index.js
+++ index.js
@@ -22,15 +22,17 @@ function cont() {
 }
 
 function bidon(p) {
-    alert(p);
+    
     var cont1 = document.getElementById("container1");
     var cont2 = document.getElementById("container2");
     var cont3 = document.getElementById("container3");
+    var cont4 = document.getElementById("container4");
     cont1.style.opacity = "0";
     cont2.style.opacity = "0";
     cont3.style.opacity = "0";
+    cont4.style.opacity = "0";
     if (p == 1) {
-        cont1.style.opacity = "0.8";
+        cont4.style.opacity = "0.8";
     }
     else if (p == 2) {
         cont2.style.opacity = "0.8";
